986749d4981c28cc553d1fe72416922846e8e79e
unknown <muhammadwaleed7450@gmail.com>
2025-10-17 14:31:51 +0500

    Lab 4 Functions

--- lab4-functions.cpp
+++ lab4-functions.cpp
@@ -12,7 +12,7 @@
 //    cin >> num;
 //    cout << "Factorial (Recursive): " << factorialRecursive(num);
 //    return 0;
-//}
+}
 
 
 
@@ -60,26 +60,26 @@
 
 
 
-//
-//#include <iostream>
-//using namespace std;
-//
-//bool isPrime(int n) {
-//    if (n <= 1) return false;
-//    for (int i = 2; i <= n / 2; i++) {
-//        if (n % i == 0) return false;
-//    }
-//    return true;
-//}
-//
-//int main() {
-//    int num;
-//    cout << "Enter a number: ";
-//    cin >> num;
-//    if (isPrime(num)) cout << "Prime Number";
-//    else cout << "Not Prime";
-//    return 0;
-//}
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if (isPrime(num)) cout << "Prime Number";
+    else cout << "Not Prime";
+    return 0;
+}
 
 
 
